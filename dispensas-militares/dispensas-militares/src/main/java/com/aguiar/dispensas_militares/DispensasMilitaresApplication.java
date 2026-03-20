584a2000d260c35bc5d781616b5661f50cb518ac
Jordan Aguiar <jordan301018@gmail.com>
2026-03-20 00:40:06 -0300

    feat: projeto inicial Spring Boot configurado

---
+++ dispensas-militares/dispensas-militares/src/main/java/com/aguiar/dispensas_militares/DispensasMilitaresApplication.java
@@ -0,0 +1,13 @@
+package com.aguiar.dispensas_militares;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DispensasMilitaresApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DispensasMilitaresApplication.class, args);
+	}
+
+}
---
+++ dispensas-militares/dispensas-militares/src/test/java/com/aguiar/dispensas_militares/DispensasMilitaresApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aguiar.dispensas_militares;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DispensasMilitaresApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

10749abcd990729dd62acd88dec5bcbf140eddb8
Jordan Aguiar <jordan301018@gmail.com>
2026-03-21 14:46:38 -0300

    feat: tela de login com identidade visual da Marinha do Brasil, SELLLLLVAAAAAA

--- dispensas-militares/dispensas-militares/src/main/java/com/aguiar/dispensas_militares/config/SecurityConfig.java
+++ dispensas-militares/dispensas-militares/src/main/java/com/aguiar/dispensas_militares/config/SecurityConfig.java
@@ -28,7 +28,7 @@ public class SecurityConfig {
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/login", "/css/**", "/js/**").permitAll()
+                        .requestMatchers("/login", "/css/**", "/js/**", "/images/**").permitAll()
                         .anyRequest().authenticated()
                 )
                 .formLogin(form -> form

4efd5a070b79700e5a4592ad097f5abef8637d86
Jordan Aguiar <jordan301018@gmail.com>
2026-03-21 01:33:40 -0300

    feat: criada entidade Militar

---
+++ dispensas-militares/dispensas-militares/src/main/java/com/aguiar/dispensas_militares/model/Militar.java
@@ -0,0 +1,28 @@
+package com.aguiar.dispensas_militares.model;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import java.time.LocalDate;
+
+@Data
+@Entity
+@Table(name = "militares")
+
+public class Militar {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String nip;
+
+    @Column(nullable = false)
+    private String nome;
+
+    @Column(nullable = false)
+    private String posto;
+
+    @Column(nullable = false)
+    private LocalDate dataInclusao;
+
+}
